7e26cf32aceb87cec188800167d26d6b1b0826a7
Vitalii_Velich <silens088@gmail.com>
2023-09-18 20:19:08 +0300

    update module

---
+++ api-tests-module/src/test/java/RestAssuredApi/Reqresapp/withPojo/PostLogin/PostLoginResponse.java
@@ -0,0 +1,21 @@
+package RestAssuredApi.Reqresapp.withPojo.PostLogin;
+
+public class PostLoginResponse {
+
+    public String token;
+
+    public PostLoginResponse(String token) {
+        this.token = token;
+    }
+
+    public PostLoginResponse() {
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
